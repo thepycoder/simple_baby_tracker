04952c1f12ebd5a5e59d31a17c8f7e117ff0711f
Victor Sonck <victor.sonck@gmail.com>
2024-04-26 07:53:00 +0200

    Moved latest cards in a separate row

--- src/App.svelte
+++ src/App.svelte
@@ -345,10 +345,8 @@
         <button
           on:click={startFood}
           class="btn btn-primary w-100"
-          disabled={isEating}
+          disabled={isEating}>Start Food Entry</button
         >
-          Start Food Entry
-        </button>
         {#if isEating}
           <div class="btn-group mt-2 w-100">
             <button
@@ -356,16 +354,14 @@
               class={foodType === "formula"
                 ? "btn btn-danger"
                 : "btn btn-outline-danger"}
-            >
-              <i class="bi bi-cup-straw"></i> Infant Formula
+              ><i class="bi bi-cup-straw"></i> Infant Formula
             </button>
             <button
               on:click={() => toggleFoodType("solid")}
               class={foodType === "solid"
                 ? "btn btn-success"
                 : "btn btn-outline-success"}
-            >
-              <i class="bi bi-apple"></i> Solid Food
+              ><i class="bi bi-apple"></i> Solid Food
             </button>
           </div>
           <div class="input-group mt-2">
@@ -386,158 +382,138 @@
               on:click={increaseAmount}>+10</button
             >
           </div>
-          <button on:click={endFood} class="btn btn-success w-100 mt-2">
-            Log Food Entry
-          </button>
+          <button on:click={endFood} class="btn btn-success w-100 mt-2"
+            >Log Food Entry</button
+          >
         {/if}
       </section>
     </div>
     <div class="row justify-content-center">
-      <!-- Sleep entries -->
+      <!-- Latest Sleep entry -->
       <div class="col-md-6">
-        {#each entries.filter((e) => e.type === "sleep") as entry}
-          <div
-            class={isLatestEntry(entry, "sleep")
-              ? "card mb-3 text-bg-success"
-              : "card mb-3"}
-          >
-            {#if $editState[entry.id]}
-              <div class="card-body">
-                <input
-                  type="datetime-local"
-                  class="form-control mb-2"
-                  bind:value={entry.edit_start}
-                  on:input={() => (entry.start = new Date(entry.edit_start))}
-                />
-                <input
-                  type="datetime-local"
-                  class="form-control mb-2"
-                  bind:value={entry.edit_end}
-                  on:input={() => (entry.end = new Date(entry.edit_end))}
-                />
-                <button
-                  class="btn btn-primary"
-                  on:click={() => saveEditSleep(entry)}>Save</button
-                >
-                <button
-                  class="btn btn-secondary"
-                  on:click={() => cancelEdit(entry.id)}>Cancel</button
-                >
-              </div>
-            {:else}
-              <div class="card-body position-relative">
-                <!-- Edit button with icon -->
-                <button
-                  on:click={() => enableEdit(entry.id)}
-                  class="btn btn-light position-absolute top-0 end-0 m-2"
-                >
-                  <i class="bi bi-pencil-square"></i>
-                </button>
-
-                <h5 class="card-title">
-                  <i class="bi bi-moon-stars"></i> Sleep
-                </h5>
-                <p class="card-text">
-                  {formatCardTime(entry.start, entry.end)}
-                </p>
-                <p class="card-text">
-                  Duration: {entry.end
-                    ? formatTime(entry.end - entry.start)
-                    : "In progress"}
-                </p>
-              </div>
-            {/if}
+        {#each entries.filter((e) => e.type === "sleep").slice(-1) as entry}
+          <div class="card mb-3 text-bg-success">
+            <div class="card-body position-relative">
+              <!-- Edit button with icon -->
+              <button
+                on:click={() => enableEdit(entry.id)}
+                class="btn btn-light position-absolute top-0 end-0 m-2"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <h5 class="card-title">
+                <i class="bi bi-moon-stars"></i> Sleep
+              </h5>
+              <p class="card-text">
+                {formatCardTime(entry.start, entry.end)}
+              </p>
+              <p class="card-text">
+                Duration: {entry.end
+                  ? formatTime(entry.end - entry.start)
+                  : "In progress"}
+              </p>
+            </div>
           </div>
         {/each}
       </div>
-      <!-- Food entries -->
+      <!-- Latest Food entry -->
       <div class="col-md-6">
-        {#each entries.filter((e) => e.type === "food") as entry}
-          <div
-            class={isLatestEntry(entry, "food")
-              ? "card mb-3 text-bg-success"
-              : "card mb-3"}
-          >
-            {#if $editState[entry.id]}
-              <div class="card-body">
-                <input
-                  type="datetime-local"
-                  class="form-control mb-2"
-                  bind:value={entry.edit_start}
-                  on:input={() => (entry.start = new Date(entry.edit_start))}
-                />
-                <input
-                  type="datetime-local"
-                  class="form-control mb-2"
-                  bind:value={entry.edit_end}
-                  on:input={() => (entry.end = new Date(entry.edit_end))}
-                />
-                <div class="btn-group mt-2 w-100 mb-2">
-                  <button
-                    on:click={() => (entry.subtype = "formula")}
-                    class={entry.subtype === "formula"
-                      ? "btn btn-light"
-                      : "btn btn-outline-light"}
-                  >
-                    <i class="bi bi-cup-straw"></i> Infant Formula
-                  </button>
-                  <button
-                    on:click={() => (entry.subtype = "solid")}
-                    class={entry.subtype === "solid"
-                      ? "btn btn-light"
-                      : "btn btn-outline-light"}
-                  >
-                    <i class="bi bi-apple"></i> Solid Food
-                  </button>
-                </div>
-                <input
-                  type="number"
-                  bind:value={entry.amount}
-                  class="form-control mb-2"
-                  placeholder="Hoeveel?"
-                />
-                <button
-                  class="btn btn-primary"
-                  on:click={() => saveEditFood(entry)}>Save</button
-                >
-                <button
-                  class="btn btn-secondary"
-                  on:click={() => cancelEdit(entry.id)}>Cancel</button
-                >
-              </div>
-            {:else}
-              <div class="card-body position-relative">
-                <!-- Edit button with icon -->
-                <button
-                  on:click={() => enableEdit(entry.id)}
-                  class="btn btn-light position-absolute top-0 end-0 m-2"
-                >
-                  <i class="bi bi-pencil-square"></i>
-                </button>
-
-                <h5 class="card-title">
-                  <i
-                    class={entry.subtype === "formula"
-                      ? "bi bi-cup-straw"
-                      : "bi bi-apple"}
-                  ></i>
-                  {entry.end
-                    ? entry.subtype === "formula"
-                      ? "Infant Formula"
-                      : "Solid Food"
-                    : "Nog bezig"}
-                </h5>
-                <p class="card-text">
-                  {formatCardTime(entry.start, entry.end)}
-                </p>
-                <p class="card-text">
-                  Amount: {entry.amount
-                    ? entry.amount +
-                      (entry.subtype === "formula" ? " ml" : " gr")
-                    : "Niet ingevuld"}
-                </p>
-              </div>
-            {/if}
+        {#each entries.filter((e) => e.type === "food").slice(-1) as entry}
+          <div class="card mb-3 text-bg-success">
+            <div class="card-body position-relative">
+              <!-- Edit button with icon -->
+              <button
+                on:click={() => enableEdit(entry.id)}
+                class="btn btn-light position-absolute top-0 end-0 m-2"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <h5 class="card-title">
+                <i
+                  class={entry.subtype === "formula"
+                    ? "bi bi-cup-straw"
+                    : "bi bi-apple"}
+                ></i>
+                {entry.end
+                  ? entry.subtype === "formula"
+                    ? "Infant Formula"
+                    : "Solid Food"
+                  : "Nog bezig"}
+              </h5>
+              <p class="card-text">
+                {formatCardTime(entry.start, entry.end)}
+              </p>
+              <p class="card-text">
+                Amount: {entry.amount
+                  ? entry.amount + (entry.subtype === "formula" ? " ml" : " gr")
+                  : "Niet ingevuld"}
+              </p>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <!-- All Sleep entries excluding the latest -->
+      <div class="col-md-6">
+        {#each entries.filter((e) => e.type === "sleep").slice(1) as entry}
+          <div class="card mb-3">
+            <div class="card-body position-relative">
+              <!-- Edit button with icon -->
+              <button
+                on:click={() => enableEdit(entry.id)}
+                class="btn btn-light position-absolute top-0 end-0 m-2"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <h5 class="card-title">
+                <i class="bi bi-moon-stars"></i> Sleep
+              </h5>
+              <p class="card-text">
+                {formatCardTime(entry.start, entry.end)}
+              </p>
+              <p class="card-text">
+                Duration: {entry.end
+                  ? formatTime(entry.end - entry.start)
+                  : "In progress"}
+              </p>
+            </div>
+          </div>
+        {/each}
+      </div>
+      <!-- All Food entries excluding the latest -->
+      <div class="col-md-6">
+        {#each entries.filter((e) => e.type === "food").slice(1) as entry}
+          <div class="card mb-3">
+            <div class="card-body position-relative">
+              <!-- Edit button with icon -->
+              <button
+                on:click={() => enableEdit(entry.id)}
+                class="btn btn-light position-absolute top-0 end-0 m-2"
+              >
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <h5 class="card-title">
+                <i
+                  class={entry.subtype === "formula"
+                    ? "bi bi-cup-straw"
+                    : "bi bi-apple"}
+                ></i>
+                {entry.end
+                  ? entry.subtype === "formula"
+                    ? "Infant Formula"
+                    : "Solid Food"
+                  : "Nog bezig"}
+              </h5>
+              <p class="card-text">
+                {formatCardTime(entry.start, entry.end)}
+              </p>
+              <p class="card-text">
+                Amount: {entry.amount
+                  ? entry.amount + (entry.subtype === "formula" ? " ml" : " gr")
+                  : "Niet ingevuld"}
+              </p>
+            </div>
           </div>
         {/each}
       </div>

d3c43ee926b446b97f1b1fd353d85d2d6d0588c7
Victor Sonck <victor.sonck@gmail.com>
2024-05-04 12:47:50 +0200

    Added base for deona cloud function

---
+++ functions/deona.py
@@ -0,0 +1,152 @@
+from dataclasses import dataclass
+from datetime import date, datetime, timedelta
+from typing import List, Optional
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+
+@dataclass
+class Slaapje:
+    start: datetime
+    end: Optional[datetime] = None
+
+
+@dataclass
+class Voeding:
+    start: datetime
+    end: datetime
+    hoeveelheid: int
+
+
+class DeonaException(Exception):
+    """An exception for bad input in Deona."""
+
+
+def get_timestamp_with_current_date(time_str):
+    # Get the current date
+    current_date = date.today()
+
+    # Parse the time string into a time object
+    time_obj = datetime.strptime(time_str, "%H:%M").time()
+
+    # Combine the date and time into a datetime object
+    timestamp = datetime.combine(current_date, time_obj)
+
+    return timestamp
+
+
+def extract_number(string):
+    # Find all the digits in the string
+    match = re.search(r"\d+", string)
+    if match:
+        # Convert the matched number to an integer
+        return int(match.group())
+    else:
+        raise DeonaException(
+            f"Geen nummer gevonden voor de hoeveelheid van het flesje! Origineel bericht: {string}"
+        )
+
+
+def read_from_deona():
+    standaard_hoeveelheid = 200
+    # Start a session
+    session = requests.Session()
+
+    # Step 1: Load the login page
+    login_url = "https://lokerenkdv.mijn-deona.be/Account/Login"
+    response = session.get(login_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Extract the anti-forgery token from the page
+    token = soup.find("input", {"name": "__RequestVerificationToken"}).get("value")
+
+    # Define login data
+    login_data = {
+        "Email": "victor.sonck@gmail.com",
+        "Password": "***REMOVED***",
+        "__RequestVerificationToken": token,
+    }
+
+    # Send a POST request to the login page
+    response = session.post(login_url, data=login_data)
+
+    # Now logged in, you can request another page
+    url_page = "https://lokerenkdv.mijn-deona.be/api/BoekjeContent/2740/1/"
+    page = session.get(url_page)
+
+    # Use Beautiful Soup to parse and extract information
+    soup = BeautifulSoup(page.content, "html.parser")
+    divs = soup.find_all("div", class_="boekjeContentRegel")
+
+    open_slaapje: Optional[Slaapje] = None
+    slaapjes: List[Slaapje] = []
+    voedingen: List[Voeding] = []
+
+    for div in divs:
+        hour = div.find("div", class_="boekjeRegelDatum").text.strip()
+        timestamp = get_timestamp_with_current_date(hour)
+        message = div.find("div", class_="boekjeRegelBericht").text.strip()
+
+        if message.startswith("Slapen"):
+            if "In slaap gevallen" in message:
+                if open_slaapje:
+                    raise DeonaException(
+                        "2 keer na elkaar in slaap gevallen zonder wakker worden!"
+                    )
+                # Start een slaapje
+                open_slaapje = Slaapje(start=timestamp)
+            elif "Wakker geworden" in message:
+                if not open_slaapje:
+                    raise DeonaException("Is wakker geworden voor te gaan slapen!")
+                # Stop een slaapje, zet het einduur, voeg het toe aan volledige slaapjes en reset het huidige slaapje
+                open_slaapje.end = timestamp
+                slaapjes.append(open_slaapje)
+                open_slaapje = None
+
+        elif message.startswith("Voeding"):
+            if "alles opgedronken!" in message:
+                # Sometime the caregiver uses "alles opgedroken" to denote a bottle completed
+                # at another time, sometime its "verder opgedronken". Decide if it is within a
+                # certain time window from the last to determine this.
+
+                # So either we're completing a bottle that was not finished before
+                if (
+                    len(voedingen) > 0
+                    and (timestamp - voedingen[-1].end) < timedelta(hours=1)
+                    and voedingen[-1].hoeveelheid < standaard_hoeveelheid
+                ):
+                    voedingen[-1].hoeveelheid = standaard_hoeveelheid
+                    voedingen[-1].end = timestamp
+
+                else:
+                    # OR the bottle was drank in one go
+                    voedingen.append(
+                        Voeding(
+                            start=timestamp - timedelta(minutes=15),
+                            end=timestamp,
+                            hoeveelheid=standaard_hoeveelheid,
+                        )
+                    )
+            elif "over" in message:
+                # Get the number, it will always be in ml left in the bottle
+                amount = extract_number(message)
+                voedingen.append(
+                    Voeding(
+                        start=timestamp - timedelta(minutes=15),
+                        end=timestamp,
+                        hoeveelheid=standaard_hoeveelheid - amount,
+                    )
+                )
+            elif "verder opgedronken" in message:
+                # Get the number, it will always be in ml left in the bottle
+                voedingen[-1].hoeveelheid = standaard_hoeveelheid
+                voedingen[-1].end = timestamp
+
+    print("\n".join([f"{s.start} {s.end}" for s in slaapjes]))
+    print("\n".join([f"{v.start} {v.end} {v.hoeveelheid}" for v in voedingen]))
+
+
+if __name__ == "__main__":
+    read_from_deona()
